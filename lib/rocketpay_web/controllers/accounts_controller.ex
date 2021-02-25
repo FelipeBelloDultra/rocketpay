a2dfb41537a11db333de4eef734ea4d75633573d
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2021-02-25 09:19:03 -0300

    feat: finish withdraw route

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -9,7 +9,15 @@ defmodule RocketpayWeb.AccountsController do
     with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
       conn
       |> put_status(:ok)
-      |> render("create.json", account: account)
+      |> render("update.json", account: account)
+    end
+  end
+
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
     end
   end
 end
